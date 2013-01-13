cb70aefbff65ab2211839061dbbbee5df04794e8
johanwannenburg <johan.wannenburg@gmail.com>
2013-01-13 20:08:01 +0200

    Update blink.cpp

--- blink.cpp
+++ blink.cpp
@@ -6,22 +6,23 @@ int main()
 {
     cout << "LED Flash start" << endl;
 
-    FILE = LEDhandle = NULL;
-    char = LEDBrightness = "/sys/class/leds/beaglebone::usr3/brightness";
+    FILE *LEDhandle = NULL;
+    char *LEDBrightness = "/sys/class/leds/beaglebone::usr3/brightness";
     
     for (int i = 0; i < 10; i++) {
         
-        if ((LEDhandle = fopen(LEDBrightness, "rw")) = NULL) {
+        if ((LEDhandle = fopen(LEDBrightness, "r+")) != NULL) {
             fwrite("1", sizeof(char), 1, LEDhandle);
-            fclose();
+            fclose(LEDhandle);
         }
         sleep(1);
 
-        if ((LEDhandle = fopen(LEDBrightness, "rw")) = NULL) {
+        if ((LEDhandle = fopen(LEDBrightness, "r+")) != NULL) {
             fwrite("0", sizeof(char), 1, LEDhandle);
-            fclose();
+            fclose(LEDhandle);
         }
         sleep(1);
+    }
     
     cout << "LED Flash end" << endl;
     return 0;
